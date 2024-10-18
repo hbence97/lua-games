91aa361ecec364e598b777962b874483033d57fc
hbence97 <hegyesbence@gmail.com>
2024-10-18 20:19:42 +0200

    [Top Down Shooter]: Add zombies as enemies

--- topDownShooter/main.lua
+++ topDownShooter/main.lua
@@ -9,6 +9,8 @@ function love.load()
   player.x = love.graphics.getWidth() / 2
   player.y = love.graphics.getHeight() / 2
   player.speed = 140
+
+  zombies = {}
 end
 
 function love.update(deltaTime)
@@ -24,15 +26,43 @@ function love.update(deltaTime)
   if love.keyboard.isDown("d") then
     player.x = player.x + player.speed * deltaTime
   end
+
+  for index, zombie in ipairs(zombies) do
+    zombie.x = zombie.x + ( math.cos(zombiePlayerAngle(zombie)) * zombie.speed * deltaTime )
+    zombie.y = zombie.y + ( math.sin(zombiePlayerAngle(zombie)) * zombie.speed * deltaTime )
+  end
 end
 
 function love.draw()
   love.graphics.draw(sprites.background)
 
   love.graphics.draw(sprites.player, player.x, player.y, playerMouseAngle(), nil, nil, sprites.player:getWidth() / 2, sprites.player:getHeight() / 2)
+
+  for i, zombie in ipairs(zombies) do
+    love.graphics.draw(sprites.zombie, zombie.x, zombie.y, zombiePlayerAngle(zombie), nil, nil, sprites.zombie:getWidth() / 2, sprites.zombie:getHeight() / 2)
+  end
 end
 
--- This function returns the angle in radians between two points
+function love.keypressed(key)
+  if key == "space" then
+    spawnZombie()
+  end
+end
+
+-- These 2 functions return the angle in radians between two points
 function playerMouseAngle()
   return math.atan2(love.mouse.getY() - player.y, love.mouse.getX() - player.x)
+end
+
+function zombiePlayerAngle(zombie)
+  return math.atan2(player.y - zombie.y, player.x - zombie.x)
+end
+
+function spawnZombie()
+  local zombie = {}
+  zombie.x = math.random(0, love.graphics.getWidth())
+  zombie.y = math.random(0, love.graphics.getWidth())
+  zombie.speed = 120
+
+  table.insert(zombies, zombie)
 end
